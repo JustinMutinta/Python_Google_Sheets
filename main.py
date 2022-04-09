d33cd1396c82318f779be46f065ea66d0aa6a081
justinmutinta <justinmutinta@hotmail.com>
2022-04-09 11:37:23 -0500

    Basic dummy source files and able to import to main

---
+++ main.py
@@ -0,0 +1,5 @@
+from source_data import user_name, favorite_movie
+
+
+print(user_name[2])
+print(favorite_movie[2])
---
+++ source_data.py
@@ -0,0 +1,10 @@
+#Source will be about person, age and favorite movie
+#Make 2 list: Name and Favorite Movie
+#Age can be random number
+#main class will import this information
+
+
+user_name = ['Sally', 'Mary', 'Susan']
+
+favorite_movie = ['Robocop', 'Terminator', 'Aliens', 'Alien']
+
