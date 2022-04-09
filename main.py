fa99dab6244c5dafcf5c123847737a86517ba20c
justinmutinta <justinmutinta@hotmail.com>
2022-04-09 11:57:44 -0500

    Added more dummy data, set up random generator

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import random
+
 from source_data import user_name, favorite_movie
 
 
 print(user_name[2])
 print(favorite_movie[2])
+
+for count in range(100):
+    movie = random.randint(0, len(favorite_movie) -1)
+    print(favorite_movie[movie])
--- source_data.py
+++ source_data.py
@@ -4,7 +4,20 @@
 #main class will import this information
 
 
-user_name = ['Sally', 'Mary', 'Susan']
+user_name = ['Sally', 'Mary', 'Susan', 'Joe', 'James',
+             'Robert', 'John', 'Michael', 'William', 'Thomas',
+             'Nancy', 'Lisa', 'Betty', 'Sandra', 'Emily',
+             'Joseph', 'Charles', 'Daniel','Steven', 'Kenneth',
+             'Carol', 'Amanda', 'Melissa', 'Kathleen', 'Helen',
+             'Timothy', 'Brian', 'Gary', 'Jacob', 'Larry',
+             'Amy', 'Pamela', 'Emma', 'Samantha', 'Nicole',]
 
-favorite_movie = ['Robocop', 'Terminator', 'Aliens', 'Alien']
+favorite_movie = ['Robocop', 'Terminator', 'Aliens', 'Alien', 'The 400 Blows',
+                  'La Haine', 'The Godfather', 'Man bites Dog', 'The Departed', 'Umberto D.',
+                  'White heat', 'Eddie Murphy: Raw', 'All Quiet on the Western Front',
+                  'Duck Soup', 'Mafioso', 'Stalag 17', 'The Shawshank Redemption', 'The Dark Knight',
+                  'Fight Club', 'Batman Begins', 'Snatch', 'Gladiator', 'Amelia',
+                  'The Good, the Bad and the Ugly', 'Saving Private Ryan', 'Coco',
+                  'America History X', 'In Bruges', 'Crash', 'Big Fish', 'Seven Samurai',
+                  'Layer Cake', 'Coming to America', 'Friday', 'Thursday',]
 
