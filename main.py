a63806674e1b296c51e63d651ac11aa3ab2f4571
justinmutinta <justinmutinta@hotmail.com>
2022-04-10 22:13:19 -0500

    Able to update the Google sheet

--- main.py
+++ main.py
@@ -7,14 +7,16 @@ sa = gspread.service_account("credentials-sheets.json")
 sh = sa.open("python-google")
 # google sheet is at: https://docs.google.com/spreadsheets/d/1LFSmJ5kSsY_qlH8EypKOFTqb3NesCN-VRxufq9jdTSE/edit#gid=0
 
-print(user_name[2])
-print(favorite_movie[2])
-"""
-for count in range(100):
+worksheet = sh.sheet1
+
+for num in range(1, 20):
     first_name = random.randint(0, len(user_name) - 1)
     last_name = random.randint(0, len(user_name) - 1)
-    movie = random.randint(0, len(favorite_movie) - 1)
+    full_name = f"{user_name[first_name]} {user_name[last_name]}"
+    movie = favorite_movie[random.randint(0, len(favorite_movie) - 1)]
     age = random.randint(10, 100)
-    #print(favorite_movie[movie])
-    print(f"{user_name[first_name]} {user_name[last_name]}, who is {age} years old, has a huge love for the movie {favorite_movie[movie]}")
-"""
+
+    worksheet.update_cell(num, 1, full_name)
+    worksheet.update_cell(num, 2, age)
+    worksheet.update_cell(num, 3, movie)
+
