44291671591b5fcf1682e6c4ddfcc9504f0a16f2
mutinta <justinmutinta@hotmail.com>
2022-04-10 21:46:17 -0500

    Changed to gspread. Able to access google sheet python-google

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import random
-import ezsheets
-
+import gspread
 
 from source_data import user_name, favorite_movie
 
+sa = gspread.service_account("credentials-sheets.json")
+sh = sa.open("python-google")
 
 print(user_name[2])
 print(favorite_movie[2])
-
+"""
 for count in range(100):
     first_name = random.randint(0, len(user_name) - 1)
     last_name = random.randint(0, len(user_name) - 1)
@@ -15,3 +16,4 @@ for count in range(100):
     age = random.randint(10, 100)
     #print(favorite_movie[movie])
     print(f"{user_name[first_name]} {user_name[last_name]}, who is {age} years old, has a huge love for the movie {favorite_movie[movie]}")
+"""
